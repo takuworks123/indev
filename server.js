5c4845357bf1fc0ea112229d89a3d5cb5af4a823
takuworks123 <107925070+takuworks123@users.noreply.github.com>
2022-07-03 15:37:36 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,13 +13,21 @@ serve(async (req) => {
   if (req.method === "POST" && pathname === "/shiritori") {
     const requestJson = await req.json();
     const nextWord = requestJson.nextWord;
+    let flg = 0;
     
     //return new Response(`${nextWord.length}`, { status: 400 });
     
-    if ( nextWord.length < 1 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0) ) {
+    if ( nextWord.length > 1 ) {
+      flg++;
+    }
+    if ( previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0) ) {
+      flg++;
+    }
+    
+    if ( flg == 2 ) {
       return new Response("前の単語に続いていません。" + nextWord.length, { status: 400 });
     }
-
+    
     previousWord = nextWord;
     return new Response(previousWord);
   }
