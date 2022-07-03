cabc74dda77b96ce5b7ad3621b6c9d7889051520
takuworks123 <107925070+takuworks123@users.noreply.github.com>
2022-07-03 15:33:32 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ serve(async (req) => {
     const requestJson = await req.json();
     const nextWord = requestJson.nextWord;
     
-    return new Response(`${nextWord.length}`, { status: 400 });
+    //return new Response(`${nextWord.length}`, { status: 400 });
     
-    if ( nextWord.length > 1 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0) ) {
+    if ( nextWord.length < 1 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0) ) {
       return new Response("前の単語に続いていません。" + nextWord.length, { status: 400 });
     }
 
