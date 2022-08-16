6271e110621d93b53b3b695794bbc03a8cc08c0a
takuworks123 <takuworks123@gmail.com>
2022-08-16 13:32:08 +0900

    coolmap

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ serve(async (req) => {
     tips.push("熱中症になった時、脇の下、首、足のつけ根を冷やしましょう。");
     tips.push("熱中症予防のために水分だけでなく、塩分もとりましょう。");
     tips.push("アイスコーヒーは水分補給にあまり適しません。");
-    tips.push("暑さ対策として、半裸で過ごすのは紫外線が直接肌に当たったり、汗を吸ってくれないのでよくありません。");
+    tips.push("暑さ対策として半裸で過ごすのは、紫外線が直接肌に当たったり、汗を吸ってくれないのでよくありません。");
     tips.push("熱中症の発生が最も多い場所は自宅です。");
     tips.push("最も熱中症になりやすい年齢層は高齢者（65歳以上）です。");
     tips.push("人間は入浴中でも汗をかき、水分が失われます。入浴前と後の両方に水分をとりましょう。");
@@ -21,10 +21,17 @@ serve(async (req) => {
     tips.push("経口補水液を作るための材料として水、塩に加えて砂糖が必要です。");
     tips.push("熱中症予防の観点から、黒色の服は熱を吸収しやすいので避けましょう。");
     tips.push("熱中症予防に最も効果的なのは、麦わら帽子です。");
+    tips.push("休憩を一回で済まさず、小分けにしましょう。");
 
     return new Response(tips[Math.floor(Math.random() * tips.length)]);
+  }
 
-    //return new Response("teeeeeeest");
+  function initMap() {
+    var opts = {
+      zoom: 15,
+      center: new google.maps.LatLng(35.6807527,139.7670716)
+    };
+    var map = new google.maps.Map(document.getElementById("map"), opts);
   }
 
   return serveDir(req, {
