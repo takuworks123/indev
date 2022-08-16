a7a6aaa96eb07cac8de91a682b408e69128db18a
takuworks123 <107925070+takuworks123@users.noreply.github.com>
2022-08-16 10:11:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// deno-lint-ignore-file
 import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 
@@ -6,15 +5,8 @@ serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
-  if (req.method === "GET" && pathname === "/tips") {
-    
-    let tips = ["お役立ち情報！！！！1"];
-    tips.push("お役立ち情報！！！！2");
-    tips.push("お役立ち情報！！！！3");
-    tips.push("お役立ち情報！！！！4");
-
-    return new Response(tips[Math.floor(Math.random() * firstWord.length)]);
-
+  if (req.method === "GET" && pathname === "/welcome-message") {
+    return new Response("jigインターンへようこそ！");
   }
 
   return serveDir(req, {
