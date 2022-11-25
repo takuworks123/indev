be6ae138afb1c55375ab06ad37adb3a1fb003f7f
takuworks123 <takuworks123@gmail.com>
2022-11-25 16:49:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -104,7 +104,6 @@ serve(async (req) => {
         .from('calendar')
         .insert({ group: `${requestJson.group}`, sche_start: `${requestJson.sche_start}`, sche_end: `${requestJson.sche_end}`, comment: `${requestJson.comment}` }); // calendarへデータ挿入
     }
-    return new Response("送信しました");
 
     if (sp.error == null) {
       return new Response("finished");
